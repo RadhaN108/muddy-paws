aae0e0d2c8af8272eb01d1d568da1207b0aea08f
RadhaN108 <RadhaN108@gmail.com>
2017-10-21 15:45:28 -0400

    fixed add to cart bug and added way for cart to store locally, started recongfiguring cart html to be table instead of divs

--- script.js
+++ script.js
@@ -7,55 +7,51 @@ function ready(docReady) {
 }
 var docReady = () => {
 
-///////////// CART JAVASCRIPT /////////////
+    ///////////// CART JAVASCRIPT /////////////
 
-// create an empty cart array
-var cart = [];
+    // create an empty cart array
+    var cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-// adding item to cart
-function addItem(name, price, color, size, count) {
-    // define variable for the item to be added
-    var item = {name: name,
-                price: price,
-                color: color,
-                size: size,
-                count: count}
-    // if cart is empty, add in the item
-    if (cart.length === 0) {
-        cart.push(item);
-        console.log(cart);
-    // if cart is not empty...
-    } else {
-        // loop through all existing items in cart, and if same item just add to item count
+    // adding item to cart
+    function addItem(name, price, color, size, count) {
+        // define variable for the item to be added
+        var item = {name: name, price: price, color: color, size: size, count: count};
+
+        // loop through all existing items in cart variable, and if same item just add to item count
+        var exists = false;
         for (var i in cart) {
             if (cart[i].name === name && cart[i].color === color && cart[i].size === size) {
                 cart[i].count += count;
                 console.log(cart);
-                return;
-            // else add the new item
-            } else {
-                cart.push(item);
-                console.log(cart);
+                exists = true;
+                break;
             }
         }
+
+        // if it doesn't exist, add new line item to cart variable
+        if(!exists) {
+            cart.push(item);
+            console.log(cart);
+        }
+
+        // updating the cart in local storage
+        localStorage.setItem('cart', JSON.stringify(cart));
     }
-}
 
 
-////////////// PRODUCT JAVASCRIPT /////////////
-// when user clicks on 'Add to Cart'...
-$("#product").submit(function( event ) {
-    // grab the product name, price, color selected, and size selected
-    var name = $(this).attr("data-name");
-    var price = $(this).attr("data-price");
-    var color = $("select#color").val();
-    var size = $("select#size").val();
-    // console.log(name, price, color, size);
-    // add item to the cart using the addItem function
-    addItem(name, price, color, size, 1);
-    event.preventDefault();
-});
-
+    ////////////// PRODUCT JAVASCRIPT /////////////
+    // when user clicks on 'Add to Cart'...
+    $("#product").submit(function( event ) {
+        // grab the product name, price, color selected, and size selected
+        var name = $(this).attr("data-name");
+        var price = $(this).attr("data-price");
+        var color = $("select#color").val();
+        var size = $("select#size").val();
+        // console.log(name, price, color, size);
+        // add item to the cart using the addItem function
+        addItem(name, price, color, size, 1);
+        event.preventDefault();
+    });
 
 }
 
