31cdc57bb5363cb0a980b179bd9fe781c158aaa6
RadhaN108 <RadhaN108@gmail.com>
2017-10-21 13:53:46 -0400

    fixed bug - adding item to cart

--- script.js
+++ script.js
@@ -20,29 +20,39 @@ function addItem(name, price, color, size, count) {
                 color: color,
                 size: size,
                 count: count}
-    // check to make sure that the item doesn't already exist in cart, if it does just add to item count
-    for (var i in cart) {
-        if (cart[i].name === name && cart[i].color === color && cart[i].size === size) {
-            cart[i].count += count;
-            return;
+    // if cart is empty, add in the item
+    if (cart.length === 0) {
+        cart.push(item);
+        console.log(cart);
+    // if cart is not empty...
+    } else {
+        // loop through all existing items in cart, and if same item just add to item count
+        for (var i in cart) {
+            if (cart[i].name === name && cart[i].color === color && cart[i].size === size) {
+                cart[i].count += count;
+                console.log(cart);
+                return;
+            // else add the new item
+            } else {
+                cart.push(item);
+                console.log(cart);
+            }
         }
-    cart.push(item);
     }
 }
 
 
 ////////////// PRODUCT JAVASCRIPT /////////////
 // when user clicks on 'Add to Cart'...
-$( "#product" ).submit(function( event ) {
+$("#product").submit(function( event ) {
     // grab the product name, price, color selected, and size selected
     var name = $(this).attr("data-name");
     var price = $(this).attr("data-price");
     var color = $("select#color").val();
     var size = $("select#size").val();
-    console.log(name, price, color, size);
+    // console.log(name, price, color, size);
     // add item to the cart using the addItem function
     addItem(name, price, color, size, 1);
-    console.log(cart);
     event.preventDefault();
 });
 
