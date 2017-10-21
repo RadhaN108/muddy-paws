9dea2378b3004e8a77e13997c695c0c75e53f4d3
RadhaN108 <RadhaN108@gmail.com>
2017-10-21 16:12:02 -0400

    adding jquery link to cart page, start restructuring cart page and styling

--- script.js
+++ script.js
@@ -9,14 +9,26 @@ var docReady = () => {
 
     ///////////// CART JAVASCRIPT /////////////
 
-    // create an empty cart array
+    ////// create an empty cart array that's in the local
     var cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    ////// find out how many items are in the cart, display on cart menu item (TO DO)
+    var itemsInCart = cart.length;
+
+    ///// show items in cart
+    // pull cart items from local storage to Items in Cart, find how many items in car
+    if (cart == null) {
+        console.log('cart is empty');
+    } else {
+        for (var i in cart) {
+            console.log('stuff in cart');
+        }
+    }
 
     // adding item to cart
     function addItem(name, price, color, size, count) {
         // define variable for the item to be added
         var item = {name: name, price: price, color: color, size: size, count: count};
-
         // loop through all existing items in cart variable, and if same item just add to item count
         var exists = false;
         for (var i in cart) {
@@ -27,19 +39,18 @@ var docReady = () => {
                 break;
             }
         }
-
         // if it doesn't exist, add new line item to cart variable
         if(!exists) {
             cart.push(item);
             console.log(cart);
         }
-
         // updating the cart in local storage
         localStorage.setItem('cart', JSON.stringify(cart));
     }
 
 
     ////////////// PRODUCT JAVASCRIPT /////////////
+
     // when user clicks on 'Add to Cart'...
     $("#product").submit(function( event ) {
         // grab the product name, price, color selected, and size selected
