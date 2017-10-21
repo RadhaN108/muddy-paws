a0d9937fe5cfd2d0f650eb8664f42c5e455f9b60
RadhaN108 <RadhaN108@gmail.com>
2017-10-21 13:33:39 -0400

    when the user clicks on add to cart, grabs the name, price, color, size of the item, bug for actually adding item to the cart

--- script.js
+++ script.js
@@ -7,13 +7,6 @@ function ready(docReady) {
 }
 var docReady = () => {
 
-////////////// PRODUCT JAVASCRIPT /////////////
-// when user clicks on 'Add to Cart'
-$(".add-to-cart").click(function(event) {
-    console.log('add to cart');
-});
-
-
 ///////////// CART JAVASCRIPT /////////////
 
 // create an empty cart array
@@ -22,7 +15,11 @@ var cart = [];
 // adding item to cart
 function addItem(name, price, color, size, count) {
     // define variable for the item to be added
-    var item = {name: name, price: price, color: color, size: size, count: count}
+    var item = {name: name,
+                price: price,
+                color: color,
+                size: size,
+                count: count}
     // check to make sure that the item doesn't already exist in cart, if it does just add to item count
     for (var i in cart) {
         if (cart[i].name === name && cart[i].color === color && cart[i].size === size) {
@@ -32,6 +29,22 @@ function addItem(name, price, color, size, count) {
     cart.push(item);
     }
 }
+
+
+////////////// PRODUCT JAVASCRIPT /////////////
+// when user clicks on 'Add to Cart'...
+$( "#product" ).submit(function( event ) {
+    // grab the product name, price, color selected, and size selected
+    var name = $(this).attr("data-name");
+    var price = $(this).attr("data-price");
+    var color = $("select#color").val();
+    var size = $("select#size").val();
+    console.log(name, price, color, size);
+    // add item to the cart using the addItem function
+    addItem(name, price, color, size, 1);
+    console.log(cart);
+    event.preventDefault();
+});
 
 
 }
