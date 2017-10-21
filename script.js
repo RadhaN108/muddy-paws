ffdca468d251e4bb4c465df35c0d13384c122a31
RadhaN108 <RadhaN108@gmail.com>
2017-10-21 12:47:27 -0400

    add jquery link to all html files, start javascript for adding to cart

--- script.js
+++ script.js
@@ -7,12 +7,31 @@ function ready(docReady) {
 }
 var docReady = () => {
 
-function thumbnailClick(thumbnail) {
+////////////// PRODUCT JAVASCRIPT /////////////
+// when user clicks on 'Add to Cart'
+$(".add-to-cart").click(function(event) {
+    console.log('add to cart');
+});
 
 
+///////////// CART JAVASCRIPT /////////////
+
+// create an empty cart array
+var cart = [];
+
+// adding item to cart
+function addItem(name, price, color, size, count) {
+    // define variable for the item to be added
+    var item = {name: name, price: price, color: color, size: size, count: count}
+    // check to make sure that the item doesn't already exist in cart, if it does just add to item count
+    for (var i in cart) {
+        if (cart[i].name === name && cart[i].color === color && cart[i].size === size) {
+            cart[i].count += count;
+            return;
+        }
+    cart.push(item);
+    }
 }
-
-
 
 
 }
