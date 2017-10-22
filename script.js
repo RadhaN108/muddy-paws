5b867cfd4ef646b481c9b77bae4bf53b1ceb7257
RadhaN108 <RadhaN108@gmail.com>
2017-10-21 23:23:52 -0400

    refactoring

--- script.js
+++ script.js
@@ -6,65 +6,77 @@ function ready(docReady) {
   }
 }
 
-///// VARIABLES  //////
+///// VARIABLES  //////////////////
 // defines the cart variable (array), pulls from local Storage if available
 var cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 
-///// FUNCTIONS  //////
-
-// adding item to cart
+///// FUNCTIONS  //////////////////
+// Adds a product to the current
+// cart array in local storage
 function addItem(name, price, color, size, count) {
-    // define variable for the item to be added
+    // define item variable
     var item = {name: name, price: price, color: color, size: size, count: count};
-    // loop through all existing items in cart variable, and if same item just add to item count
+    // loop through all existing items in cart, and if the item exists, the +1 to item count
     var exists = false;
     for (var i in cart) {
         if (cart[i].name === name && cart[i].color === color && cart[i].size === size) {
             cart[i].count += count;
-            console.log(cart);
             exists = true;
-            break;
+            break; // stop loop when item is found
         }
     }
-    // if it doesn't exist, add new line item to cart variable
+    // if item doesn't exist in cart, add new line item
     if(!exists) {
         cart.push(item);
         console.log(cart);
     }
-    // updating the cart in local storage
+    // update the cart in local storage
     localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-// updating number in Cart Menu
+// update the number of items in cart
+// that displays on the cart menu item
 function updateCartMenu() {
+    // get the cart menu item
     var cartMenuItem = $(".cart");
+    // if there's no items in cart, then no number
     if (cart.length == 0) {
         cartMenuItem.html("Cart");
+        hideCartElements();
     } else {
-        cartMenuItem.html("Cart (" + cart.length + ")");
-        console.log(cartMenuItem);
+        // otherwise, find the quantity of products in the cart
+        var cartQuant = 0;
+        for (var i in cart) {
+            cartQuant += cart[i].count;
+        }
+        // update the cart menu item html
+        cartMenuItem.html("Cart (" + cartQuant + ")");
     }
 }
 
-// checking if cart is empty to hide unnecessary cart elements
+// checking if cart is empty to
+// hide unnecessary cart elements
 function hideCartElements() {
+    // hide check out button
     $(".check-out").remove();
+    // hide cart table
     $("#cart-table").remove();
 }
 
-///// RUNS WHEN DOC IS READY  //////
+///// WHEN DOC IS READY... //////////////////
 var docReady = () => {
+    // show number of items in cart in the menu
+    updateCartMenu();
 
-    ///// show items in cart
-    // get the cartTable on the cart page
+    //////// CART /////////
+    // get the table on the cart page and create an empty html var
     var cartTable = $("#cart-table");
-
-    var html = "";
-    // if there's no items in the cart, don't do anything
+    var rowsHtml = "";
+    // if there's no items in the cart, hide the checkout button and table
     if (cart.length == 0) {
         hideCartElements();
-    } else { // if there are items in the cart, then add the rows to the cart page
+    } else { // otherwise,  if the cart has items
         for (var i in cart) {
             // get data for each line item
             var name = cart[i].name;
@@ -73,38 +85,39 @@ var docReady = () => {
             var size = cart[i].size;
             var count = cart[i].count;
             // append it to the html to be added, 1 row for each line item
-            html += "<tr class='item'><td><div class='cart-product-image' style='background-image: url(img/cat-harness0.jpg)''></div><h3>" + name +"</h3></td><td><p>Color:  " + color + "</p><p>Size:  " + size + "</p></td><td><p>" + count + "</p></td><td><a href='#'>modify item</a><a href='#' class='remove'>remove item</a></td></tr>";
-            updateCartMenu();
-            $(".check-out").show();
+            rowsHtml += "<tr class='item'><td><div class='cart-product-image' style='background-image: url(img/cat-harness0.jpg)''></div><h3>" + name +"</h3></td><td><p>Color:  " + color + "</p><p>Size:  " + size + "</p></td><td><p>" + count + "</p></td><td><a href='#'>modify item</a><a href='#' class='remove'>remove item</a></td></tr>";
         }
-        // add the html to the table
-        cartTable.append(html);
+        // add the html rows to the table
+        cartTable.append(rowsHtml);
+        // show the check-out button
+        $(".check-out").show();
     }
 
-    // when user clicks to remove a product...
+    // when the remove button is clicked...
     $(".remove").on("click", function(e) {
+        // get the index of the table row
         var index = $(this).parent().parent().index(".item");
+        // remove the row from the table
         $(this).parent().parent().remove();
+        // remove the row from the cart array and update teh local storage
         cart.splice(index, 1);
         localStorage.setItem('cart', JSON.stringify(cart));
+        // update the cart menu item with current products
         updateCartMenu();
-        if (cart.length == 0) {
-            hideCartElements();
-        };
     });
 
-    ////////////// PRODUCT PAGE JAVASCRIPT /////////////
+    //////// PRODUCT PAGE /////////
 
     // when user clicks on 'Add to Cart'...
     $("#product").submit(function(e) {
-        // grab the product name, price, color selected, and size selected
+        // grab the product name, price, selected color, and select size
         var name = $(this).attr("data-name");
         var price = $(this).attr("data-price");
         var color = $("select#color").val();
         var size = $("select#size").val();
+        // add item to the cart array
         addItem(name, price, color, size, 1);
     });
-
 }
 
 ready(docReady)
